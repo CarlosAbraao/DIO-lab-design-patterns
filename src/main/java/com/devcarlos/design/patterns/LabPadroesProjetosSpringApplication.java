13d14a33abc2dd9e6a83ae7cc298966e14c4f844
Carlos Abraao <carloschavesnd@hotmail.com>
2022-08-11 17:23:04 -0300

    inicio do projeto

---
+++ src/main/java/com/devcarlos/design/patterns/LabPadroesProjetosSpringApplication.java
@@ -0,0 +1,13 @@
+package com.devcarlos.design.patterns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabPadroesProjetosSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabPadroesProjetosSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devcarlos/design/patterns/LabPadroesProjetosSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devcarlos.design.patterns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabPadroesProjetosSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
